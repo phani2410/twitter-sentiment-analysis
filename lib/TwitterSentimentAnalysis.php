f941d92808aa96792826422e4b6d492275b404e8
PHANINDRA SURA <sura.phanindra@gmail.com>
2017-01-20 12:33:21 +0530

    change in include

--- lib/TwitterSentimentAnalysis.php
+++ lib/TwitterSentimentAnalysis.php
@@ -1,6 +1,8 @@
 <?php
-include_once(dirname(__FILE__).'/DatumboxAPI.php');
-include_once(dirname(__FILE__).'/twitter-client.php');
+$ds = DIRECTORY_SEPARATOR;
+$base_dir = realpath(dirname(__FILE__)  . $ds . '..') . $ds;
+require_once("{$base_dir}DatumboxAPI.php");
+require_once("{$base_dir}twitter-client.php");
 class TwitterSentimentAnalysis {
     
     protected $datumbox_api_key; //Your Datumbox API Key. Get it from http://www.datumbox.com/apikeys/view/
