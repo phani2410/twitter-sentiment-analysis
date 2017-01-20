1755cc5e2b2523a5bc6a9d7b972377de629341b8
PHANINDRA SURA <sura.phanindra@gmail.com>
2017-01-20 11:32:32 +0530

    key update

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
 // Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
-define('DATUMBOX_API_KEY', '');
+define('DATUMBOX_API_KEY', '360ac15ac073b2c7be90cad432ee99ed');
 
 // Configure your OAuth settings from your application at https://dev.twitter.com/apps
 
-define('TWITTER_CONSUMER_KEY', '');
-define('TWITTER_CONSUMER_SECRET', '');
+define('TWITTER_CONSUMER_KEY', 'JXHk32PCHZ2vAbNWTWKUHHjig');
+define('TWITTER_CONSUMER_SECRET', 'QreWkUOkk6rNcHyeH9pXXXT8Fia2UlnQkhcgeQADRIPPo3jvmW');
 
 
 // Configure authentication credentials.
 // you can generate your own access key from the link above
 
-define('TWITTER_ACCESS_KEY', '');
-define('TWITTER_ACCESS_SECRET', ''); 
+define('TWITTER_ACCESS_KEY', '313132193-pCaSq95cxYO93zL0VIeyNJDBYjlE7UCpi1VCnnZr');
+define('TWITTER_ACCESS_SECRET', 'zR7TPOAbJOcBf4xb6sT7VJurnDs2UsMCisBfYTe5v4JL1'); 
